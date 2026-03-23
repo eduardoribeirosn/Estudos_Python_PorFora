ab94c07d8e3caf341c97031aa80f1e561729fe75
Eduardo-xX <eduardo.nascimento@sptech.school>
2026-03-23 09:36:58 -0300

    Salvando alterações anteriores.

--- EstudosLangGraph/Coisas-ChatGPT/projetos/projetos-acompanhados/mini-chatbot/inicial.py
+++ EstudosLangGraph/Coisas-ChatGPT/projetos/projetos-acompanhados/mini-chatbot/inicial.py
@@ -1,4 +1,10 @@
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, END
+from typing import TypedDict
+
+class State(TypedDict):
+    message: str
+    response: str
+    resultSoma: int
 
 def chatbot(state):
     message = state["message"]
@@ -10,27 +16,38 @@ def chatbot(state):
     elif "boa noite" in message:
         return {"response": "boa noite"}
     elif "soma" in message:
-        return {"response": message}
-    return {"response": f"Você disse: {message}"}
+        return {"response": "soma"}
+    elif "subtrair" in message:
+        return {"response": "sub"}
+    return {"response": "fim"}
 
 def soma(state):
-    content = state["response"]
+    content = state["message"]
     itens = content.split()
-    print(itens[1] + itens[2])
-    return itens[1] + itens[2]
 
-# def condSoma(state):
-#     content = state["response"]
-#     if content in "soma":
-#         return "soma"
-#     else:
-#         return "fim"
+    a = int(itens[1])
+    b = int(itens[2])
 
-graph = StateGraph(dict)
+    resultado = a + b
+    print(resultado)
+    return {"resultSoma": resultado}
+
+def subtrair(state):
+    content = state["message"]
+    itens = content.split()
+
+    a = int(itens[1])
+    b = int(itens[2])
+
+    resultado = a - b
+    print(resultado)
+    return {"resultSub": resultado}
+
+graph = StateGraph(State)
 
 graph.add_node("chatbot", chatbot)
-
 graph.add_node("soma", soma)
+graph.add_node("sub", subtrair)
 
 # graph.add_node("condSoma", condSoma)
 
@@ -40,16 +57,18 @@ graph.add_conditional_edges(
     "chatbot",
     lambda state: state["response"],
     {
-        "soma", "soma",
-        "fim", "END"
+        "soma": "soma",
+        "sub": "sub",
+        "fim": END
     }
-
 )
+
+graph.add_edge("soma", END)
 
 app = graph.compile()
 
 result = app.invoke({
-    "message": "soma 25 20"
+    "message": "subtrair 10 3"
 })
 
 print(result)
